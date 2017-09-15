efcd241ca0b1c33c00f43e797f2d2050c6ae692f
Carolina Díaz Gómez <astridc.diaz@udea.edu.co>
2017-09-15 07:33:46 -0500

    07:33

--- src/operacionespolinomios/Operaciones.java
+++ src/operacionespolinomios/Operaciones.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vista;
+package operacionespolinomios;
 
 /**
  *
- * @author Felipe
+ * @author Carolina
  */
-public class Principal extends javax.swing.JFrame {
+public class Operaciones extends javax.swing.JFrame {
 
     /**
-     * Creates new form Principal
+     * Creates new form Operaciones
      */
-    public Principal() {
+    public Operaciones() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Operaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Operaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Operaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Operaciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Principal().setVisible(true);
+                new Operaciones().setVisible(true);
             }
         });
     }
